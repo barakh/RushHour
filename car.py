cd23e3069d3a28703535c1d9b94061e58abdb2a7
Barak <brkhad@gmail.com>
2016-03-19 10:33:50 +0200

    Modified the car module to have better handling of acceleration and deceleration

--- car.py
+++ car.py
@@ -12,6 +12,14 @@ import argparse
 UDP_SERVER_IP = "127.0.0.1"
 UDP_SERVER_PORT = 5000
 
+"""
+Car parameters
+"""
+TURNING_ANGLE = 0.2
+ACCELERATION = 0.2
+DECELERATION = -0.5
+MAX_SPEED = 2
+
 class Car(object):
     def __init__(self, start_point = (50,200), 
                  start_angle = 0, 
@@ -26,6 +34,22 @@ class Car(object):
         x = (self.speed * cos(self.angle)) + self.pos[0]
         y = (self.speed * sin(self.angle)) + self.pos[1]
         self.pos = (x,y)
+    
+    def right(self):
+        self.angle += TURNING_ANGLE
+
+    def left(self):
+        self.angle -= TURNING_ANGLE
+
+    def speed_up(self):
+        self.speed += ACCELERATION
+        if self.speed > MAX_SPEED:
+            self.speed = MAX_SPEED
+        
+    def slow_down(self):
+        self.speed += DECELERATION
+        if self.speed < 0:
+            self.speed = 0
 
     def __repr__(self):
         return pformat((self.listening_port, self.pos, self.angle)) + "\n"
@@ -51,13 +75,13 @@ def main_loop():
             data, addr = sock_input.recvfrom(1024) # buffer size is 1024
             if data != "":
                 if "right" in data:
-                    c.angle += 0.2
+                    c.right()
                 elif "left" in data:
-                    c.angle -= 0.2
+                    c.left()
                 elif "up" in data:
-                    c.speed += 1
+                    c.speed_up()
                 elif "down" in data:
-                    c.speed -= 1
+                    c.slow_down()
         except socket.error:
             pass
         time.sleep(0.03)
