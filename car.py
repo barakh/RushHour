3f784d22d9b9b7dd0c9aa88820fd9c78b0d32ea9
Barak <brkhad@gmail.com>
2016-05-20 09:05:05 +0300

    Adde API to update car location and send location to the monitor server

--- car.py
+++ car.py
@@ -25,10 +25,15 @@ class Car(object):
                  start_angle = 0, 
                  speed = 0,
                  listening_port = 0):
-        self.pos = start_point
-        self.angle = float(start_angle)
-        self.speed = speed
+        self.set_location(start_point, start_angle, speed)
         self.listening_port = listening_port
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                             socket.SOCK_DGRAM) # UDP
+
+    def set_location(self, pos, angle, speed):
+        self.pos = pos
+        self.angle = angle
+        self.speed = speed
 
     def make_step(self):
         x = (self.speed * cos(self.angle)) + self.pos[0]
@@ -54,6 +59,9 @@ class Car(object):
     def __repr__(self):
         return pformat((self.listening_port, self.pos, self.angle)) + "\n"
 
+    def send_car_data(self):
+        self.sock.sendto(repr(self), (UDP_SERVER_IP, UDP_SERVER_PORT))
+
 def main_loop(): 
     listening_port = randint(10000,30000)
     sock_input = socket.socket(socket.AF_INET, # Internet
@@ -61,8 +69,6 @@ def main_loop():
     sock_input.bind((UDP_SERVER_IP, listening_port))
     sock_input.setblocking(False)
 
-    sock = socket.socket(socket.AF_INET, # Internet
-                         socket.SOCK_DGRAM) # UDP
     c = Car(speed = 1.0, 
             start_point = (randint(100,500), randint(100,400)),
             listening_port = listening_port)
@@ -70,7 +76,7 @@ def main_loop():
         #TODO: Make this code pretty
         c.make_step()
         c.angle -= (0.5 - random.random()) / 10
-        sock.sendto(repr(c), (UDP_SERVER_IP, UDP_SERVER_PORT))
+        c.send_car_data()
         try:
             data, addr = sock_input.recvfrom(1024) # buffer size is 1024
             if data != "":
