8e2831d61138ad4bc10e63eef7929ee7e0d127e7
gmnimrod <gmnimrod@gmail.com>
2016-05-31 21:16:21 +0300

    Added get_positions.py. This is camera+detection api.

--- camera_detection.py
+++ camera_detection.py
@@ -1,6 +1,7 @@
 import cv2
 from detection import Detector
 from camera import Camera
+from get_positions import  HEIGHT_BOUNDS, WIDTH_BOUNDS
 
 cam = Camera()
 im_ind = 0
@@ -9,6 +10,8 @@ while(1):
         frame = cam.getCurrentFrame()
 
         if type(frame) != type(None):
+            # Crop frame in order to filter out areas that are off of the white surface.
+            frame = frame[HEIGHT_BOUNDS[0]:HEIGHT_BOUNDS[1], WIDTH_BOUNDS[0]:WIDTH_BOUNDS[1]]
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
             detector = Detector()
